88f5d4eafdb2413572ab18deef4b76b601b4d58c
Riku Kuisma <130903460+Swifterhtmler@users.noreply.github.com>
2026-04-17 01:10:04 +0300

    added logic to add snippets to local array

--- SnippetMagnet Extension/Resources/popup.js
+++ SnippetMagnet Extension/Resources/popup.js
@@ -1,30 +1,42 @@
-let naming = document.getElementById('codebox');
-      let codeNames = [];
-       //  let textbox = document.getElementsByClassName('textbox');
-      let textboxtext = document.getElementById('textbox');
+let map = new Map();
 
-      textboxtext.addEventListener('keypress', function(e) {
-          if (e.key === 'Enter') {
-              e.preventDefault();
-              textboxtext.value = '';
-              textboxtext.style.display = 'none'
-             // naming.focus();
-          //    document.getElementById('codebox').focus();
-        
-          }
-      })
-       
+function Snippet(name,code) {
+   this.name = name,
+   this.code = code
+}
+
+ let naming = document.getElementById('codebox');
+ let codeNames = [];
+  //  let textbox = document.getElementsByClassName('textbox');
+ let textboxtext = document.getElementById('textbox');
 
 
-      naming.addEventListener('keypress', (e) => {
-          if (e.key === 'Enter') {
-              e.preventDefault(); // Prevent form submission
-              codeNames.push(naming.value)
-              // clear the textfield
-              naming.value = '';
-              document.getElementById('textbox').style.display = 'block';
-              document.getElementById('textbox').focus()
-              console.log(codeNames)
-      
-          }
-      });
+
+ textboxtext.addEventListener('keypress', function(e) {
+     if (e.key === 'Enter') {
+         e.preventDefault();
+         textboxtext.style.display = 'none'
+         const newSnippet = new Snippet(naming.value,textboxtext.value);
+         codeNames.push(newSnippet)
+         // console.log(newSnippet);
+         console.log(codeNames);
+         naming.value = '';
+         textboxtext.value = '';
+   
+     }
+ })
+  
+
+ naming.addEventListener('keypress', (e) => {
+     if (e.key === 'Enter') {
+         e.preventDefault(); // Prevent form submission
+       //  codeNames.push(naming.value)
+         // clear the textfield
+         document.getElementById('textbox').style.display = 'block';
+         document.getElementById('textbox').focus()
+         console.log
+         
+ 
+     }
+ });
+
