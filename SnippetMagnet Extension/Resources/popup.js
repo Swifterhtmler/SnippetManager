221d14a25a48cdf2fb23bcb7188876d3e9cc4aa7
Riku Kuisma <130903460+Swifterhtmler@users.noreply.github.com>
2026-04-23 01:58:08 +0300

    added new js logic to localstorage

--- SnippetMagnet Extension/Resources/popup.js
+++ SnippetMagnet Extension/Resources/popup.js
@@ -1,107 +1,119 @@
 
-    let map = new Map();
+ let map = new Map();
 
-    let visibility = false;
+ let visibility = false;
 
-    function Snippet(name,code) {
-       this.name = name,
-       this.code = code
+ function Snippet(name,code) {
+    this.name = name,
+    this.code = code
+ }
+
+  let naming = document.getElementById('codebox');
+  let codeNames = [];
+   //  let textbox = document.getElementsByClassName('textbox');
+  let textboxtext = document.getElementById('textbox');
+
+  let listActivationButton = document.getElementById('listactivation');
+
+
+  let listElementList = document.getElementById('listelements')
+
+  let hasRun = false;
+
+
+  // add items to list
+
+  // hide the ui depending on state
+
+   listActivationButton.addEventListener('click', function clicked() {
+    let list = document.getElementById('listelements');
+    if (list.style.display === "none" ) {
+        list.style.display = "block";
+    } else {
+        list.style.display = "none";
     }
 
-     let naming = document.getElementById('codebox');
-     let codeNames = [];
-      //  let textbox = document.getElementsByClassName('textbox');
-     let textboxtext = document.getElementById('textbox');
-
-     let listActivationButton = document.getElementById('listactivation');
+  });
 
 
-     let listElementList = document.getElementById('listelements')
+  // add items to list
 
-     let hasRun = false;
+  textboxtext.addEventListener('keypress', function(e) {
+      if (e.key === 'Enter') {
+          e.preventDefault();
+          textboxtext.style.display = 'none'
+          const newSnippet = new Snippet(naming.value,textboxtext.value);
+          codeNames.push(newSnippet);
+          // console.log(codeNames);
+
+     var name = document.createElement('div');
+     var code = document.createElement('div');
+     var divUnit = document.createElement('div');
+      divUnit.className = "divUnit";
+      code.className = "code";
+      name.className = "name";
 
 
-     // add items to list
+      name.innerHTML = newSnippet.name;
+      code.innerHTML = newSnippet.code;
 
-     // hide the ui depending on state
+      divUnit.appendChild(name);
+      divUnit.appendChild(code);
+      listElementList.appendChild(divUnit);
 
-      listActivationButton.addEventListener('click', function clicked() {
-       let list = document.getElementById('listelements');
-       if (list.style.display === "none" ) {
-           list.style.display = "block";
-       } else {
-           list.style.display = "none";
-       }
-
-     });
-
- 
+         // localstorage save arrays
+        console.log(codeNames);
+        localStorage.setItem("naming", JSON.stringify(codeNames));
+   //   localStorage.setItem("newItems",JSON.stringify(newSnippet));
+      
+          naming.value = '';
+          textboxtext.value = '';
+          document.getElementById('codebox').focus();
     
+      }
 
+  })
+
+
+  naming.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+          e.preventDefault(); // Prevent form submission
+          // clear the textfield
+          document.getElementById('textbox').style.display = 'block';
+          document.getElementById('textbox').focus();
+  
+      }
+  });
 
   
-
-     // add items to list
-
-     textboxtext.addEventListener('keypress', function(e) {
-         if (e.key === 'Enter') {
-             e.preventDefault();
-             textboxtext.style.display = 'none'
-             const newSnippet = new Snippet(naming.value,textboxtext.value);
-             codeNames.push(newSnippet);
-             // console.log(codeNames);
-
-        var name = document.createElement('div');
-        var code = document.createElement('div');
-
-         name.innerHTML = newSnippet.name;
-         code.innerHTML = newSnippet.code;
-
-         listElementList.appendChild(name);
-         listElementList.appendChild(code);
-
-            // localstorage save arrays
-           console.log(codeNames);
-           localStorage.naming = JSON.stringify(codeNames);
-      //   localStorage.setItem("newItems",JSON.stringify(newSnippet));
-         
-             naming.value = '';
-             textboxtext.value = '';
-             document.getElementById('codebox').focus();
-       
-         }
-
-     })
+// onload function
+window.addEventListener("load", function () {
+       // get the list
+    var storedNames = JSON.parse(localStorage.getItem("naming"));
+       //console.log(storedNames);
+    
+  if (storedNames) {
+      codeNames = storedNames;
+       // render list when page loads
+       storedNames.forEach(function(element) {
+       console.log("forEach running")
+       var divUnit = document.createElement('div');
+       divUnit.className = "divUnit";
+       var name = document.createElement('div');
+       var code = document.createElement('div');
+       code.className = "code";
+       name.className = "name";
 
 
-     naming.addEventListener('keypress', (e) => {
-         if (e.key === 'Enter') {
-             e.preventDefault(); // Prevent form submission
-             // clear the textfield
-             document.getElementById('textbox').style.display = 'block';
-             document.getElementById('textbox').focus();
-     
-         }
-     });
-     
-  // onload function
- window.addEventListener("load", function () {
-             
-          // get the list
-          var storedNames = JSON.parse(localStorage.naming);
-          //console.log(storedNames);
+       name.innerHTML = element.name;
+       code.innerHTML = element.code;
 
-          // render list when page loads
-          storedNames.forEach(function(element) {
-          console.log("forEach running")
-          var name = document.createElement('div');
-          var code = document.createElement('div');
+       divUnit.appendChild(name);
+       divUnit.appendChild(code);
+       listElementList.appendChild(divUnit);
+       });
+    }
 
-          name.innerHTML = element.name;
-          code.innerHTML = element.code;
 
-          listElementList.appendChild(name);
-          listElementList.appendChild(code);
-          });
-  });
+});
 
